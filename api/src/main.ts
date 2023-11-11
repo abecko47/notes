14a5ed82156f4756b7dd9c3b0540612396ff8ea0
Anatoliy <anatoliy.kotsur@seasy.at>
2023-11-11 16:30:16 +0100

    - add prisma module & service - add swagger docs at /apiref

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle('3IPK Notes')
+      .setDescription('3IPK Notes API')
+      .setVersion('1.0')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('apiref', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
