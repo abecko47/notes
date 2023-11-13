237e2029f9f0ddbfad9cb0849c38719fdd4ba70e
Anatoliy <anatoliy.kotsur@seasy.at>
2023-11-13 09:52:45 +0100

    added some tests to seach to test the transformation of search query to tsQuery

--- api/src/search/search.service.spec.ts
+++ api/src/search/search.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SearchService } from './search.service';
 import { PrismaService } from '../prisma/prisma.service';
+import {queryToTsQueryWithAnd, queryToTsQueryWithOr} from "./util/searchUtil";
 
 describe('SearchService', () => {
   let service: SearchService;
@@ -15,5 +16,15 @@ describe('SearchService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('test transformation to tsQuery with and', () => {
+    const searchQuery = "my note ";
+    expect(queryToTsQueryWithAnd(searchQuery)).toEqual("my:* & note:*")
+  });
+
+  it('test transformation to tsQuery with or', () => {
+    const searchQuery = "my note ";
+    expect(queryToTsQueryWithOr(searchQuery)).toEqual("my:* | note:*")
   });
 });
